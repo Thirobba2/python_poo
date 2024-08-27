ade6792ad27bc18d17d88df9d74694d2fe04dcd4
Thirobba2 <thiago.robba.oliveira@hotmail.com>
2024-08-26 21:37:48 -0300

    fix archive

--- src/main.py
+++ src/main.py
@@ -1,60 +1,79 @@
 from frota import *
+import pickle
 
-
-def operar_carro(carro : Carro):
-    print('1- Ligar motor')
-    print('2- Desligar motor')
-    print('3- Acelerar')
+def operar(opcao : Carro):
+    print(f'1- Ligar motor do {opcao.modelo}')
+    print(f'2- Desligar motor do {opcao.modelo}')
+    print(f'3- Acelerar do {opcao.modelo}')
 
     op = 0
     while op not in (1, 2, 3):
         op = int(input("Digite as opcoes[1-3]: "))
 
     if op == 1:
-        carro.ligar()
+        opcao.ligar()
     elif op == 2:
-        carro.desligar()
+        opcao.desligar()
     elif op == 3:
-        v = float(input("Informe a velocidade: "))
-        t = float(input("Informe o tempo: "))
-        carro.acelerar(v, t)
+        v = float(input(f"Informe a velocidade do {opcao.modelo}: "))
+        t = float(input(f"Informe o tempo do {opcao.modelo}: "))
+        opcao.acelerar(v, t)
+
+    print('Infos atuais do carro')
+    print(opcao)
 
 if __name__ == "__main__":
-    print('Cadastre um carro')
+    print('Cadastre o carro 1')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    nm_consumoMedio = float(input('Digite o consumo medio: '))
+    nm_tanque = float(input('Digite o nivel do tanque: '))
 
-    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro1 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, nm_tanque, nm_consumoMedio)
 
-    print('Cadastre um carro')
+    print('Cadastre o carro 2')
     nm_modelo = input('Digite o modelo: ')
     nm_marca = input('Digite a marca: ')
     nm_cor = input('Digite a cor: ')
+    nm_consumoMedio = float(input('Digite o consumo medio: '))
+    nm_tanque = float(input('Digite o nivel do tanque: '))
 
-    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False)
+    carro2 = Carro(nm_modelo, nm_marca, nm_cor, 0, False, nm_tanque, nm_consumoMedio)
+
+    carros = {}
+    carros[id(carro1)] = carro1
+    carros[id(carro2)] = carro2
+
+    try:
+        with open('carros.pkl', 'wb') as arquivo:
+            pickle.dump(carros, arquivo)
+    except Exception as e:
+        print(e)
 
     '''
     Controlando o carro até ele atingir 10000 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600:
+
+    while carro1.getOdometro() < 300 and carro2.getOdometro() < 300 and (carro1.getTanque() > 0 or carro2.getTanque() > 0):
         try:
-            op = 0
-            while op not in (1, 2):
-                op = int(input("Qual carro [1, 2]? "))
+            print('Qual carro deseja utilizar ?')
 
-            if op == 1:
-                operar_carro(carro1)
-            else:
-                operar_carro(carro2)
+            opCarro = 0
+            while opCarro not in (1, 2):
+                opCarro = int(input('1 ou 2: '))
 
+                if opCarro == 1:
+                    operar(carro1)
+                elif opCarro == 2:
+                    operar(carro2)
+                else:
+                    raise Exception("Opcao invalida!!")
 
         except Exception as e:
             print("Erro!")
             print(e)
 
     carro1.desligar()
-    carro2.desligar()
     print(carro1)
-    print(carro2)
-    print("Fim da Corrida")
+    print('Parar para trocar óleo!!!')
